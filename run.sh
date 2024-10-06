09755a8344ecd6732e5ea84d309b5c4a8fb9cc55
Carlo Acosta <acostacarlom@gmail.com>
2024-10-06 16:42:15 +0800

    Added socat port forwarding for Linux

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "Starting project..."
 dotnet run --project llm-credit-score-api/llm-credit-score-api.csproj &
 python3 mock-llm-server/main.py &
-wait
+sudo socat TCP-LISTEN:443,fork TCP:localhost:5000 &
