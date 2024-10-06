0aa471e5cc75bfe03ece5a5c552bee1954969b6c
Carlo Acosta <acostacarlom@gmail.com>
2024-10-06 16:50:32 +0800

    Keep previous code

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 echo "Starting project..."
 dotnet run --project llm-credit-score-api/llm-credit-score-api.csproj &
 python3 mock-llm-server/main.py &
-sudo socat TCP-LISTEN:443,fork TCP:localhost:5000 &
